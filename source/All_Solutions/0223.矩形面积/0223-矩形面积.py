7ac72354a3e52a47de60929c83998abdd77c1062
Ziming M <mzm@mail.dlut.edu.cn>
2020-01-27 20:00:29 +0800

    original commit

---
+++ source/All_Solutions/0223.矩形面积/0223-矩形面积.py
@@ -0,0 +1,18 @@
+class Solution:
+    def computeArea(self, A: int, B: int, C: int, D: int, E: int, F: int, G: int, H: int) -> int:
+        # 调整两个矩形位置, 让第一个矩形靠最左边
+        if A > E:
+            return self.computeArea(E, F, G, H, A, B, C, D)
+        # 没有重叠的情况
+        if B >= H or D <= F or C <= E:
+            return abs(A - C) * abs(B - D) + abs(E - G) * abs(F - H)
+        # 重叠情况
+        # 下边界
+        down = max(A, E)
+        # 上
+        up = min(C, G)
+        # 左
+        left = max(B, F)
+        # 右
+        right = min(D, H)
+        return abs(A - C) * abs(B - D) + abs(E - G) * abs(F - H) - abs(up - down) * abs(left - right)
